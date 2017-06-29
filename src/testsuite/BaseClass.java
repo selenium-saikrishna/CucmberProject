2a2c896635c68b7cca664c618efd48765e998cc6
sai krishna <selenium.saikrishna@gmail.com>
2017-06-29 07:07:49 -0700

    Second Commit

--- src/testsuite/BaseClass.java
+++ src/testsuite/BaseClass.java
@@ -3,7 +3,7 @@ package testsuite;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BaseClass {
-	
+	System.out.println("Creating driver object");
 	static FirefoxDriver driver=null;
 
 }
